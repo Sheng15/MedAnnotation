0dc0bf54fe98282b8272ecd4901cc5509c2dac8b
sheng15 <tspower@qq.com>
2018-10-28 16:02:36 +1100

    fileProcesser

--- fileProcesser.py
+++ fileProcesser.py
@@ -1,143 +1,181 @@
-from treelib import Node, Tree
+##author Sheng Tang
+##15/09/2018  10：23
+
 import sys
 import os
+import datetime
 
 import meshTree
 
-class Token(object):
-	def __init__(self,tag,start,end):
-		self.tag = tag
-		self.start = start
-		self.end = end
 
-def readAnnotation(file):
-	##T1\tDrug 1000 1025\tanticoagulant medications\nT2\tDrug 3620 3628\tdilaudid\n
-	##R1\tRoute-Drug Arg1:T3 Arg2:T2
-	A = []
-	B = []
-	C = []
-	D = []
-	E = []
-	F = []
-	G = []
-	H = []
-	I = []
-	J = []
-	K = []
-	L = []
-	M = []
-	N = []
-	V = []
-	Z = []
-	otherToken = []
-	tokens = [] 
-	relations = list()
-	predications = list()
-	with open(file,'r') as f:
-		content = f.read()
-	lines = content.split('\n')
-	for i in range(len(lines)-1): ##aviod last line, where has noting
-		splitedLine=lines[i].split('\t')
-		index = splitedLine[0]
+###########################################################################################################
+#############				Methods that can be used to save file into database				  ############# 
+#############				inclouding：save raw file or process it for query				  #############
+###########################################################################################################
+#python3
 
-		if(index[0]=='T'):###tokens
-			category=splitedLine[1].split(' ')[0]
-			start = splitedLine[1].split(' ')[1]
-			end = splitedLine[1].split(' ')[2]
-			token = splitedLine[2]
-			tokenConverted = token.title()
-			if meshTree.containsToken(tokenConverted):
-				nids = meshTree.meshDic[tokenConverted]			
-				if  ('A' in nids[0]):
-					A.append(token)
-				elif('B' in nids[0]):
-					B.append(token)
-				elif('C' in nids[0]):
-					C.append(token)
-				elif('D' in nids[0]):
-					D.append(token)
-				elif('E' in nids[0]):
-					E.append(token)
-				elif('F' in nids[0]):
-					F.append(token)
-				elif('G' in nids[0]):
-					G.append(token)
-				elif('H' in nids[0]):
-					H.append(token)
-				elif('I' in nids[0]):
-					I.append(token)
-				elif('J' in nids[0]):
-					J.append(token)
-				elif('K' in nids[0]):
-					K.append(token)
-				elif('L' in nids[0]):
-					L.append(token)
-				elif('M' in nids[0]):
-					M.append(token)
-				elif('N' in nids[0]):
-					N.append(token)
-				elif('V' in nids[0]):
-					V.append(token)
-				else:
-					Z.append(token)
-			else:
-				otherToken.append(token)
-			tokens.append({"index":index,"category":category,"start":start,"end":end,"token":token})
-		else:
-			relation = splitedLine[1].split(' ')[0]
-			Arg1 = splitedLine[1].split(' ')[1]
-			Arg2 = splitedLine[1].split(' ')[2]
-			relations.append({"index":index,"relation":relation,"Arg1":Arg1,"Arg2":Arg2})
-			if relation not in predications:
-				predications.append(relation)
+##################################################################################################
+######################  methods to read clinical trials and annotation files  ####################
+##################################################################################################
+
+
+def readFile(file):
+	'''
+	read a file with the given path
+	Parameters
+	• file 	- path of the file to be read
+	returns a string of the content in the target file
+	'''
+	f = open(file,'r')
+	result = ''
+	for line in f.readlines():
+		result = result+line
 	f.close()
-	partitions = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,V,Z,otherToken]
-	return tokens,relations,predications,partitions
+	return result
 
-def readText(file):
+def countWord(file):
+	'''
+	count the word of a given file
+	Parameters
+	• file 	- path of the file to be read
+	returns a int as the word count 
+	'''
 	wordCount = 0
 	with open(file, 'r') as f:
 		content = f.read()
-	lines = content.split('\n')
+	lines = content.split('\n')##remove all “\n” symbols
 	for line in lines:
 		words = line.split()
 		wordCount += len(words)
 	f.close()
 	return wordCount
 
-def containsList(tree,nids):
-	for nid in nids:
-		if tree.contains(nid):
-			return True	
-	return False
 
-def saveInTree(tokens):
-	tokensNotInTree = []
-	tree = Tree()
-	tree.create_node('Root','root')##root
+def computeID(text,user):
+	'''
+	user mmh3 hash to compute an identifier of a clinical trial and its annotation
+	Parameters
+	• text 	- string content of a text
+	• user 	- identifier of the user， usually the user name
+	returns a string of number as the identifier
+	'''
+	text_id = str(mmh3.hash(text,signed=False))
+	user_id = str(mmh3.hash(user,signed=False))
+	id = text_id+user_id
+	return id
 
-	for token in tokens:
-		tokenConverted = token['token'].title()
-		if meshTree.containsToken(tokenConverted):##containsToken(token)
-			##print(tokenConverted)
-			nids = meshTree.meshDic[tokenConverted]
-			if(containsList(tree,nids)):
-				leaf = Node(token["token"],token["index"],data = [token["index"],token["category"],token["start"],token["end"]])
-				tree.add_node(leaf,'root')
-			else:
-				ancestorsList = meshTree.getAncestors(tokenConverted)
-				for ancestors in ancestorsList:
-					for i in range(len(ancestors)-1):
-						if(not(tree.contains(ancestors[i]))):##contains(nid)
-							node = Node(meshTree.getTag(ancestors[i]),ancestors[i])
-							parent = ancestors[i-1]
-							tree.add_node(node,parent)
-					leaf = Node(token["token"],ancestors[-1],data = [token["index"],token["category"],token["start"],token["end"]])
-					tree.add_node(leaf,ancestors[-2])
+
+
+##################################################################################################
+######################  process and save clinical trials and annotation files  ###################
+##################################################################################################
+def updateDic(token,dic):
+	'''
+	update the appearance frequency dict，the dict is something like {"aspirin"：2，"dilaudid"：1}
+	Parameters
+	• token - the key of the dict to be updated
+	• dic 	- the appearance frequency dict
+	'''
+	if token in dic.keys():
+			num = dic[token] +1
+			dic[token] = num
 		else:
-			tokensNotInTree.append(token)
+			dic[token] = 1
 
-	return tree,tokensNotInTree
+def save_annotated_text(server,user,db,clinical_trial,annotation_file):
+
+	##T1\tDrug 1000 1025\tanticoagulant medications\nT2\tDrug 3620 3628\tdilaudid\n
+	##R1\tRoute-Drug Arg1:T3 Arg2:T2
+
+	####step1.compute identifier
+	text = readFile(clinical_trial)
+	annotation_ann = readFile(annotation_file)
+	id = computeID(text,user)
+
+	####step2.count word of clinical trial
+	wordsCount = fileProcesser.readText(clinical_trial)
 
 
+	###step3.process annotation file，save the frequency of appearance of category,token,relation into a dict
+	tokensDic = {}
+	tokenIndex = {}
+	categoryDic = {}
+	relations = []
+	predicationsDic = {}
 
+	with open(annotation_file,'r') as f:
+		content = f.read()
+	lines = content.split('\n') ## every line in the annotation file
+	for i in range(len(lines)-1): ##aviod last line, where has noting
+		splitedLine=lines[i].split('\t')
+		index = splitedLine[0]    ## T or R
+
+		if(index[0]=='T'):###tokens
+			## update the dict of category
+			category = splitedLine[1].split(' ')[0].lower()
+			updateDic(category,categoryDic)
+			## update the dict of tokens
+			token = splitedLine[2].lower() ### transfer all character to lower case
+			tokenIndex[index] = token
+			updateDic(token,tokensDic)
+		else:
+			##relations
+			relation = splitedLine[1].split(' ')[0].lower()
+			Arg1Index = splitedLine[1].split(' ')[1].split(":")[1].lower()
+			Arg2Index = splitedLine[1].split(' ')[2].split(":")[1].lower()
+			relations.append({"relation":relation,"Arg1":Arg1Index,"Arg2":Arg2Index})
+
+
+	#### for a relation in annotation file, such as "R8	Form-Drug Arg1:T16 Arg2:T15"
+	#### translate the index of the token "Arg1:T16" to the particular token it indicates
+	for item in relations:
+		arg1 = tokenIndex[item["Arg1"]]
+		arg2 = tokenIndex[item["Arg2"]]
+		relation = item["relation"]
+
+		predications = [arg1+relation,relation+arg2,arg1+relation+arg2]
+
+		for predicate in predications:
+			updateDic(predicate,predicationsDic)
+
+
+	date = datetime.date.today()
+
+	####step4.generate the two documents body to be saved to database
+	_doc1 = {
+			"User":user,
+			"Clinical_Trial":text,
+			"Annotation":annotation_ann,
+			"Date":date
+			}
+			
+	_doc2 = {
+			"User":user,
+			"Category":categoryDic,
+			"Tokens":tokensDic,
+			"Predications":predicationsDic,
+			"WordsCount":wordsCount
+			}
+
+	####step5.save to database
+	try:
+		fileDatabase = server[db]
+		searchDatabase = server[db+"search"]
+		fileDatabase[id] = doc1
+		searchDatabase[id] = doc2
+	except Exception as e:
+		print("Failed to save file "+clinical_trial)
+
+	####step6.update stats
+	try:
+		stats = searchDatabase.get("stats")
+		wordsCount = stats["WordsCount"] + 1
+		docsCount = stats["DocsCount"] + 1
+		stats["WordsCount"] = wordsCount
+		stats["DocsCount"] = docsCount
+		searchDatabase.save(stats)
+	except Exception as e:
+		print("Failed to update stats")
+		
+		
+	
