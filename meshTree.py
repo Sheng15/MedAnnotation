cbd0d2e810bc227140730e9b88dff720938cfd47
sheng15 <tspower@qq.com>
2018-10-28 19:47:52 +1100

    meshtree

--- meshTree.py
+++ meshTree.py
@@ -1,11 +1,21 @@
+##author Sheng Tang
+##15/09/2018  10：43
+
+'''
+process the mesh dictionary to a useable tree structure
+for more detial of mesh, see "https://www.nlm.nih.gov/mesh/""
+the document of treelib can be found at "https://treelib.readthedocs.io/en/latest/pyapi.html"
+'''
 from treelib import Node, Tree
 
-
-TREE_PATH = "mtrees2018.bin"
-
-CATAGORY = ["Anatomy","Organisms","Diseases","Chemicals and Drugs","Analytical, Diagnostic and Therapeutic Techniques and Equipment","Psychiatry and Psychology","Phenomena and Processes","Disciplines and Occupations",
-"Anthropology, Education, Sociology and Social Phenomena","Technology, Industry, Agriculture","Humanities","Information Science","Named Groups","Health Care","Publication Characteristics","Geographicals"]
 '''
+path of mesh.bin file download from "ftp://nlmpubs.nlm.nih.gov/online/mesh/2018/meshtrees/",which will be updated annually.
+'''
+TREE_PATH = "mtrees.bin"
+
+
+'''
+an enumerate of the categories in mesh(in lower case)
 A. Anatomy
 B. Organisms
 C. Diseases
@@ -23,8 +33,19 @@ N. Health Care
 V. Publication Characteristics 
 Z. Geographicals
 '''
+CATAGORY = ["anatomy","organisms","diseases","Cchemicals and drugs","analytical, diagnostic and therapeutic techniques and equipment","psychiatry and psychology","phenomena and processes","disciplines and occupations",
+"anthropology, education, sociology and social phenomena","technology, industry, agriculture","humanities","information science","named groups","health care","publication characteristics","geographicals"]
 
-def readTree(TREE_PATH):
+#####################################################################
+######################  construct the mesh tree  ####################
+#####################################################################
+def readTree():
+	'''
+	read the mesh tree from the TREE_PATH
+	retruns an arrry of all the tokens in the mesh tree in lower case
+	a token there is a list that include its term and its tree id 
+	for example [["body regions,"a01"],[anatomic landmarks","a01.111]]
+	'''
 	tokens = []
 
 	with open(TREE_PATH,'rb') as f:
@@ -32,11 +53,19 @@ def readTree(TREE_PATH):
 		contents = data.decode('utf-8').split('\n')
 
 	for content in contents:
-		token = content.split(';')
+		token = content.split(';').lower()
 		tokens.append(token)
-	return tokens[:-1]
+	return tokens[:-1]##the last token is null
 
 def creatTree(tokens):
+	'''
+	construct a tree object of mesh tree, every node in the tree is a node object,
+	wich has two attributes,nid and tag, refer treelib document for more info.
+	nid refers to the treeid and tag refers to the term.
+	Parameters
+	• tokens - the tokens read from meshtree file
+	retruns an tree object of mesh tree
+	'''	
 	meshTree = Tree()
 
 	meshTree.create_node('Root','root')##root
@@ -44,45 +73,52 @@ def creatTree(tokens):
 		meshTree.create_node(category,category,parent = 'root')##initial the tree
 
 	for token in tokens:
-		if(not'.' in token[1]): ## tokens with ids like 'A01' that are the elder sons of each category
-			if  ('A' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Anatomy')
-			elif('B' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Organisms')
-			elif('C' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Diseases')
-			elif('D' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Chemicals and Drugs')
-			elif('E' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Analytical, Diagnostic and Therapeutic Techniques and Equipment')
-			elif('F' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Psychiatry and Psychology')
-			elif('G' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Phenomena and Processes')
-			elif('H' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Disciplines and Occupations')
-			elif('I' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Anthropology, Education, Sociology and Social Phenomena')
-			elif('J' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Technology, Industry, Agriculture')
-			elif('K' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Humanities')
-			elif('L' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Information Science')
-			elif('M' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Named Groups')
-			elif('N' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Health Care')
-			elif('V' in token[1]):
-				meshTree.create_node(token[0],token[1],parent = 'Publication Characteristics')
+		if(not'.' in token[1]): ## tokens with ids like 'a01' that are the elder sons of each category
+			if  ('a' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'anatomy')
+			elif('b' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'organisms')
+			elif('c' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'diseases')
+			elif('d' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'chemicals and drugs')
+			elif('e' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'analytical, diagnostic and therapeutic techniques and equipment')
+			elif('f' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'psychiatry and psychology')
+			elif('g' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'phenomena and processes')
+			elif('h' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'disciplines and occupations')
+			elif('i' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'anthropology, education, sociology and social phenomena')
+			elif('j' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'technology, industry, agriculture')
+			elif('k' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'humanities')
+			elif('l' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'information science')
+			elif('m' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'named groups')
+			elif('n' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'health care')
+			elif('v' in token[1]):
+				meshTree.create_node(token[0],token[1],parent = 'publication characteristics')
 			else:
-				meshTree.create_node(token[0],token[1],parent = 'Geographicals')
+				meshTree.create_node(token[0],token[1],parent = 'geographicals')
 		else:
 			parent_id = token[1][:-4]
 			meshTree.create_node(token[0],token[1],parent= parent_id)
 	return meshTree
 
-def createDictionary(tokens):
+def meshDict(tokens):
+	'''
+	create a dict object of mesh tree, some term in the tree might have multiple tree id
+	for example {"term1":["treeid1","treeid2","treeid3"]}
+	Parameters
+	• tokens - the tokens read from meshtree file
+	retruns a dict, keys are the terms in mesh with their treeids as value
+	'''	
 	dic = {}
 
 	for token in tokens:
@@ -99,36 +135,75 @@ def createDictionary(tokens):
 
 
 def loadMeshTree():
+	'''
+	use the methods above to load a mesh tree,
+	returns a tree object and a dict object of mesh
+	and you are ready to use mesh now!
+	'''
 	tokens = readTree(TREE_PATH)			
 	meshTree = creatTree(tokens)
-	meshDic = createDictionary(tokens)
+	meshDic = meshDict(tokens)
 	return meshTree,meshDic
 
 
+################################################################################## 
+################### methods to use mesh tree and mesh dict########################
 ##################################################################################
 meshTree,meshDic = loadMeshTree()
 
 def notRoot(nid):
+	'''
+	check whether a node is the root of the meshtree
+	Parameters
+	• nid - the nid of a node
+	retruns true if not root
+	'''	
 	if(nid==meshTree.root):
 		return False
 	else:
 		return True
 
 def containsToken(token):
-	if token in meshDic.keys():
+	'''
+	check whether a token can be found in meshtree
+	Parameters
+	• token - the token to be check
+	retruns true if the token is found
+	'''
+	if token.lower() in meshDic.keys():
 		return True
 	else:
 		return False
 
 def getTag(nid):
+	'''
+	get the node's tag by its nid
+	Parameters
+	• nid - the nid of a node
+	retruns tag of the node
+	'''	
 	return meshTree.get_node(nid).tag
 
 def getnids(token):
+	'''
+	get the token's nids,it might more than one nid because a term might has multiple treeid
+	Parameters	
+	• token - the token to be check
+	retruns a list of nids
+	'''	
 	if (containsToken(token)):
 		nids = meshDic[token]
 	return nids
 
 def getAncestors(token):
+	'''
+	get the token's ancestors in mesh,there might be more than one nid,ancestors of each nid are expended
+	Parameters	
+	• token - the token to be check
+	retruns a list of nids
+
+	in this project, most time we dont care about the ancestors of a token. 
+	'''	
 	ancestorsList=[]
 
 	if (containsToken(token)):
@@ -146,7 +221,19 @@ def getAncestors(token):
 	return ancestorsList
 
 
+
+
 def getDescendants(token,distance = False):
+	'''
+	get the token's descendants in mesh,there might be more than one nid,decendants of each nid are expended
+	Parameters	
+	• token 	- the token to be check
+	• distance  – (distance = False, not return the distance between a token and its descendants)
+	for a hierarchy   A -> B -> C -> D,the distance would be {"A":0,"B":1,"C":2,"D":3}
+	retruns a list of descendants
+
+	in this project, use this method to support semantic search
+	'''	
 	if distance == False:
 		descendants = []
 		if (containsToken(token)):
@@ -177,24 +264,3 @@ def getDescendants(token,distance = False):
 						descendants[node.tag] = subTree.level(node.identifier)
 		return descendants
 
-
-
-
-
-def getTokenAncestors(token):
-	ancestorsList=[]
-
-	if (containsToken(token)):
-		nids = meshDic[token]
-		for nid in nids:
-			ancestors = []	
-			while(notRoot(nid)):
-				parent = meshTree.parent(nid)
-				ancestors.append(parent.tag)
-				nid = parent.identifier
-			ancestors = list(ancestors[:-2])
-			ancestorsList.append(ancestors)
-	else:
-		print("cannot find "+token+" in the MeSH!")
-
-	return ancestorsList
